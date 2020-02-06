ff59ba0a5f2a3fea6eb26943620040d9f3e35da4
Devorah Levi <devorahlevi1@gmail.com>
2020-02-05 21:42:05 -0600

    Added command line argument processing.

--- src/main/java/edu/bi/foo/HelloWorld.java
+++ src/main/java/edu/bi/foo/HelloWorld.java
@@ -3,6 +3,10 @@ package edu.bi.foo;
 public class HelloWorld {
 
     public static void main (String[]args){
-        System.out.println("Hello World!");
+        String name = "Bob";
+        if (args.length > 0){
+            name = args[0];
+        }
+        System.out.println("Hello, " + name + " from the world!");
     }
 }
